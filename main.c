6b8b629cf6beee01158f037896c17fa2a2c32ae2
Ramita Tr <loogmai@Loogmais-MacBook-Pro.local>
2019-03-07 00:37:25 -0800

    Initialzed PWM

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ void disable_interrupts(void);
 void enable_interrupts(void);
 void wait_for_interrupts(void);
 
+void PWM_Init(void);
+
 volatile unsigned long count = 0;
 volatile unsigned long In, Out;
 unsigned int TOGGLE_COUNT = 1000;
@@ -17,11 +19,11 @@ volatile int dis_blue = 0;
 
 /* main */
 int main(void){
-  PLL_Init();                 // bus clock at 80 MHz
   PortF_Init();
+  PWM_Init();
   count = 0;
 
-  SysTick_Init(80000);        // initialize SysTick timer
+  SysTick_Init(16000);        // initialize SysTick timer
   enable_interrupts();
 
   while(1){                   // interrupts every 1ms
@@ -54,6 +56,28 @@ void PortF_Init(void) {
     enable_interrupts();
 }
 
+void PWM_Init(void){
+    SYSCTL_RCGC0_R |= 0x00100000;       // 1) Activate PWM clock
+    SYSCTL_RCGC2_R |= 0x00000002;       // 2) Activate clock for PortB
+    while((SYSCTL_PRGPIO_R&0x02) == 0)  // Wait until PortB is ready for access
+    {};
+    GPIO_PORTB_AFSEL_R |= 0x40;         // 3) Enable alt funct on PB6 (0100.0000)
+    GPIO_PORTB_DEN_R |= 0x40;           // enable digital I/O on PB6
+    GPIO_PORTB_PCTL_R &= ~0x0F000000;   // Clear PMC6 (Port Mux Control 6)
+    GPIO_PORTB_PCTL_R |= 0x04000000;    // 4) Configure PB6 as PWM
+    SYSCTL_RCC_R |= 0x00100000;           // 5) Use PWM divider
+    SYSCTL_RCC_R &= ~0x000E0000;        // Clear PWMDIV
+    SYSCTL_RCC_R |= 0x00060000;         // Set divider /16: SysClock = 16MHz, PWM clock = 1MHz
+    PWM0_0_CTL_R = 0;                   // 6) Configure PWM generator for count-down mode
+    PWM0_0_GENA_R = 0x8C;               // Drive pwmA High on Load, low on CMPA down
+    //PWM0_0_GENB_R = 0x80C;              // Drive pwmA High on Load, low on CMPB down
+    PWM0_0_LOAD_R = 10000 - 1;          // 7) Set period (-1 for counting down to 0)
+                                        // (PWM clock source 8MHz / Desire PWM frequency 100MHz)
+                                        // 80000 ticks per period
+    PWM0_0_CMPA_R = 10000/2 - 1;        // 8) Set the pulse width (zero duty cycle)
+    PWM0_0_CTL_R |= 0x00000001;         // 9) Start PWM0
+    PWM0_ENABLE_R |= 0x00000001;        // 10) Enable PWM outputs
+}
 
 /* Disable interrupts by setting the I bit in the PRIMASK system register */
 void disable_interrupts(void) {
@@ -80,21 +104,13 @@ void wait_for_interrupts(void) {
 // Executed every 12.5ns*(period)
 // TOGGLE_COUNT = 1000 call SysTick Interrupt every 1 sec
 void SysTick_Handler(void){
-    count++;
-    if (count == TOGGLE_COUNT - 1) {
-        count = 0;
-        // toggle bit at PF2
-        if (!dis_blue){
-            GPIO_PORTF_DATA_R ^= 0x04;
-        }
-        PortF = GPIO_PORTF_DATA_R;
-    }
+
 }
 
 // Interrupt service routine for PF4
 void GPIO_Handler(void){
     GPIO_PORTF_ICR_R = 0x10;    // Acknowledge Flag for PF4
-    dis_blue ^= 1;
-    GPIO_PORTF_DATA_R &= ~0x04; // CLear PF4
-    GPIO_PORTF_DATA_R ^= 0x02;  // Toggle PF2
+    temp++;
+    PWM0_0_CMPA_R-=1000;
+
 }
