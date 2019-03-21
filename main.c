e339c6f513c4456b1fe0c4d2a85b8e564382bad3
Ramita Tr <loogmai39@gmail.com>
2019-03-20 23:35:02 -0700

    Include on-board LED display

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 void DAC_Init(void);
 void PortA_Init(void);
 void PortE_Init(void);
+void PortF_Init(void);
 void disable_interrupts(void);
 void enable_interrupts(void);
 void wait_for_interrupts(void);
@@ -15,7 +16,7 @@ volatile unsigned char Index = 0;           // Index varies from 0 to 31
 
 volatile unsigned long count = 0;
 volatile unsigned long In, Out;
-const unsigned long note[8] = {19084,17007,15152,14327,12755,11364,10121,9560}; //C,D,E,F,G,A,B,C
+const unsigned long note[8] = {9582,8503,7576,7163,6377,5682,5060,4780}; //C,D,E,F,G,A,B,C
 volatile unsigned long period;
 
 
@@ -24,6 +25,7 @@ int main(void){
     PLL_Init();
     PortE_Init();
     PortA_Init();
+    PortF_Init();
     DAC_Init();
     period = note[0];
     SysTick_Init(period);        // initialize SysTick timer
@@ -74,6 +76,21 @@ void PortE_Init(void) {
     GPIO_PORTE_DEN_R |= 0x0F;               // Enable digital I/O on PE3-0
 }
 
+/* Initialize PortF GPIOs */
+void PortF_Init(void) {
+    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF;           // activate clock for PortF
+    while ((SYSCTL_PRGPIO_R & SYSCTL_RCGC2_GPIOF) == 0)
+    {};                          // wait until PortF is ready
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;         // unlock GPIO PortF
+    GPIO_PORTF_CR_R |= 0x0F;                 // allow changes to PF3-0
+    GPIO_PORTF_AMSEL_R &= 0x0F;              // disable analog on PF3-0
+    GPIO_PORTF_PCTL_R &= ~0x0000FFFF;         // use PF4-0 as GPIO
+    GPIO_PORTF_DIR_R |= 0x0F;                // configure PF3-0 out
+    GPIO_PORTF_AFSEL_R &= ~0x0F;              // disable alt function on PF3-0
+    GPIO_PORTF_PUR_R &= ~0x0F;                // disable pull-up on PF3-0
+    GPIO_PORTF_DEN_R |= 0x0F;                // enable digital I/O on PF3-0
+}
+
 /* Disable interrupts by setting the I bit in the PRIMASK system register */
 void disable_interrupts(void) {
     __asm("    CPSID  I\n"
@@ -108,27 +125,35 @@ void SysTick_Handler(void){
     if (GPIO_PORTE_DATA_R&0x0F || GPIO_PORTA_DATA_R&0x3C){
         if (GPIO_PORTE_DATA_R&0x01) {
             period = note[0];
+            GPIO_PORTF_DATA_R = 0x02;
         }
         else if (GPIO_PORTE_DATA_R&0x02) {
             period = note[1];
+            GPIO_PORTF_DATA_R = 0x04;
         }
         else if (GPIO_PORTE_DATA_R&0x04) {
             period = note[2];
+            GPIO_PORTF_DATA_R = 0x08;
         }
         else if (GPIO_PORTE_DATA_R&0x08) {
             period = note[3];
+            GPIO_PORTF_DATA_R = 0x0A;
         }
         else if (GPIO_PORTA_DATA_R&0x04) {
             period = note[4];
+            GPIO_PORTF_DATA_R = 0x0C;
         }
         else if (GPIO_PORTA_DATA_R&0x08) {
             period = note[5];
+            GPIO_PORTF_DATA_R = 0x0E;
         }
         else if (GPIO_PORTA_DATA_R&0x10) {
             period = note[6];
+            GPIO_PORTF_DATA_R = 0x06;
         }
         else if (GPIO_PORTA_DATA_R&0x20) {
             period = note[7];
+            GPIO_PORTF_DATA_R = 0x02;
         }
         DAC_Out(SineWave[Index]);
         Index = (Index+1)&0x0F;
@@ -136,5 +161,6 @@ void SysTick_Handler(void){
     }
     else {
         DAC_Out(0);
+        GPIO_PORTF_DATA_R = 0x00;
     }
 }
